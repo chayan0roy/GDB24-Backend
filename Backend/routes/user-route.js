23b70cde40d5a54a5acd89ace6a2a6cdf54b7b68
Chayan Roy <tochayanroy@gmail.com>
2025-03-15 21:51:47 +0530

    Multer Crus Operation

--- Backend/routes/user-route.js
+++ Backend/routes/user-route.js
@@ -9,6 +9,11 @@ const {singleImageUpload} = require('../middleware/multer')
 
 
 
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const unlinkAsync = promisify(fs.unlink);
+
 
 
 
@@ -107,6 +112,74 @@ router.get('/get-profile', passport.authenticate('jwt', { session: false }),
         }
     }
 );
+
+
+
+
+
+
+
+
+
+
+
+
+router.put('/update-profile-picture', passport.authenticate('jwt', { session: false }), singleImageUpload,  async (req, res) => {
+    try {
+        const user = req.user;
+
+        if (user.image) {
+            const oldImagePath = path.join(__dirname, '..', 'uploads', user.image);
+            if (fs.existsSync(oldImagePath)) {
+                await unlinkAsync(oldImagePath);
+            }
+        }
+
+
+        user.image = req.file.filename;
+        await user.save();
+
+        res.status(200).json({ message: 'Profile picture updated successfully', image: req.file.filename });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+
+
+
+router.delete('/delete-profile-picture', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    try {
+        const user = req.user;
+
+        if (user.image) {
+            const imagePath = path.join(__dirname, '..', 'uploads', user.image);
+            
+            if (fs.existsSync(imagePath)) {
+                await unlinkAsync(imagePath);
+            }
+
+            user.image = null;
+            await user.save();
+
+            return res.status(200).json({ message: 'Profile picture deleted successfully' });
+        } else {
+            return res.status(400).json({ message: 'No profile picture found' });
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+
+
+
+
+
 
 module.exports = router;
 
